194b1fa44f657823ce6c06cddff69d665c061da9
Joe Ryan <joe@clanryan.us>
2024-03-08 21:17:47 -0500

    added check to ensure valid version returned for facts

--- lib/facter/frontier-squid-majver.rb
+++ lib/facter/frontier-squid-majver.rb
@@ -3,8 +3,11 @@ require 'facter'
 
 version = Facter::Util::Resolution.exec("rpm -q frontier-squid --queryformat '[%{NAME} %{VERSION}-%{RELEASE}\n]'")
 
-Facter.add("#{version.split[0]}_majver".gsub('-','_')) do
-    setcode do
-          "#{version.split[1].split('.')[0]}"
+if version
+    Facter.add("#{version.split[0]}_majver".gsub('-','_')) do
+        setcode do
+            "#{version.split[1].split('.')[0]}"
+        end
+
     end
 end
